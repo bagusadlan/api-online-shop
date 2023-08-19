ee3c0b230441bdfee67dd6e19c0cd394f04a6dea
bagusadlan <bagusadlann@gmail.com>
2023-08-20 00:57:45 +0700

    dev(register): add new policy for register

---
+++ src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,35 @@
+const Joi = require('joi')
+
+module.exports = {
+  register(req, res, next) {
+    const schema = Joi.object({
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    })
+
+    const { error, value } = schema.validate(req.body)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'Anda harus memberikan alamat email yang valid'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'Anda harus memberikan password yang valid'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Informasi pendaftaran tidak valid'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
   app.get('/', () => {
     console.log('home');
   })
-  
+  app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
+
 }
