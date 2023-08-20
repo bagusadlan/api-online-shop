71c262c51e6c2584e2f6e5d4f24268fc4a75658c
bagusadlan <bagusadlann@gmail.com>
2023-08-20 08:18:34 +0700

    dev(config): add pool database in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,20 @@
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'toko-online',
+    database: process.env.DB_NAME || 'kelas.com-toko-online',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     options: {
       host: process.env.HOST || 'localhost',
-      dialect: process.env.DIALECT || 'mysql'
-    }
+      dialect: process.env.DIALECT || 'mysql',
+      
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    },
   },
   authentication: {
     jwtSecret: process.env.JWT_SECRET || 'secret'
