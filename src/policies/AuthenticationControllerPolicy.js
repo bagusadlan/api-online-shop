5702d6474ebea396757e71ad6c26436294b42f75
bagusadlan <bagusadlann@gmail.com>
2023-08-20 13:46:11 +0700

    feat(register): add name & username validation in Auth Policy

--- src/policies/AuthenticationControllerPolicy.js
+++ src/policies/AuthenticationControllerPolicy.js
@@ -3,6 +3,8 @@ const Joi = require('joi')
 module.exports = {
   register(req, res, next) {
     const schema = Joi.object({
+      name: Joi.string(),
+      username: Joi.string(),
       email: Joi.string().email(),
       password: Joi.string().regex(
         new RegExp('^[a-zA-Z0-9]{8,32}$')
